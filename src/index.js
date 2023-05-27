013dba814f50a81be2b1114a1ce66a150bad76cf
santicano <canosantiago404@gmail.com>
2023-05-27 16:07:57 -0300

    Añadido formulario para buscar ciudad, solo con console.log

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import getWeather from "./modules/api";
+import { containerForm, userEntry } from "./modules/dom";
 
-document.addEventListener('DOMContentLoaded', getWeather('Buenos Aires'));
-getWeather('London');
+let input = 'Buenos Aires'
+
+document.addEventListener('DOMContentLoaded', getWeather(input));
+
+containerForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    input = userEntry.value;
+
+    getWeather(input);
+});
---
+++ src/modules/dom.js
@@ -0,0 +1,4 @@
+const containerForm = document.querySelector('.container-form');
+const userEntry = document.querySelector('[new-entry]');
+
+export {containerForm, userEntry};
