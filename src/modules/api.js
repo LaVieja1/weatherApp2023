d1b46775843e6248bc9e8cf91bae4dca6e24e50f
santicano <canosantiago404@gmail.com>
2023-05-27 23:48:56 -0300

    fix https

--- src/modules/api.js
+++ src/modules/api.js
@@ -2,7 +2,7 @@ import displayWeather from "./displayWeather";
 
 export default async function getWeather(location) {
     try {
-        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=68c6d26bf9ff4e4b97013316232705&q=${location}`, {mode: 'cors'});
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=68c6d26bf9ff4e4b97013316232705&q=${location}`, {mode: 'cors'});
         const weatherData = await response.json();
         console.log(weatherData);
         displayWeather(weatherData);
